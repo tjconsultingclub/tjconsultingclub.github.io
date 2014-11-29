ffa8f89cdd111c954bea481c3f52f09a10a5a4d5
unknown <Tjconsultingclub@gmail.com>
2014-11-28 22:36:58 -0500

    optimized for mobile

--- Scripts/collective.js
+++ Scripts/collective.js
@@ -28,3 +28,17 @@ function deEmphasizeBlock(id)
 {
 		TweenMax.to($("#"+id) , .2, {opacity:1, ease:Power1.easeInOut});
 }
+function showSlideBar() {
+		TweenMax.to($('#slideBar') ,0, {display:'block', ease:Power1.easeInOut});
+		TweenMax.to($('#slideBar') ,  .3, {right:'0%', ease:Power1.easeInOut});	
+}
+function hideSlideBar(){
+	TweenMax.to($('#slideBar') ,  .4, {right:'-1000%', ease:Power1.easeInOut});
+	TweenMax.to($('#slideBar') ,.4, {display:'none', ease:Power1.easeInOut});
+}
+function highlightSideBar(id){
+	TweenMax.to($("#"+id) , .3, {background:"#010243", ease:Power1.easeInOut});
+}
+function unlightSideBar(id){
+	TweenMax.to($("#"+id) , .3, {background:"none", ease:Power1.easeInOut});
+}
